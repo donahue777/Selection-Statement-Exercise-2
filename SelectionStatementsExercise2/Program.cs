3c3752b3990f18302ae5cc01d902bef139b1e60c
Thomas Donahue <donahue777@yahoo.com>
2023-10-01 09:33:51 -0400

    Exercise 2 complete

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What's your favorite subject in school?");
+            
+            var subject = Console.ReadLine();
+
+            switch (subject.ToLower())
+            {
+                case "math":
+                    Console.WriteLine("Math? Nice.");
+                    break;
+                case "english":
+                    Console.WriteLine("English? Nice.");
+                    break;
+                case "history":
+                    Console.WriteLine("History? Nice.");
+                    break;
+                case "science":
+                    Console.WriteLine("Science? Nice.");
+                    break;
+                case "psychology":
+                    Console.WriteLine("Psychology? Nice.");
+                    break;
+                default:
+                    Console.WriteLine($"{subject}!!! Nice.");
+                    break;
+            }
         }
     }
 }
